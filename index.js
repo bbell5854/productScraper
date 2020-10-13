d1d882259a979eeee2a81f7d4de34e0e6c68abe0
Brad Bell <bbell5854@gmail.com>
2020-10-12 21:13:50 -0400

    Adding ps5

--- index.js
+++ index.js
@@ -17,31 +17,26 @@ function sendNotification(message, url) {
   axios.post(pushoverUrl, payload);
 }
 
-function checkEvga3080() {
-  const htmlUrls = [
-    'https://www.evga.com/products/product.aspx?pn=10G-P5-3885-KR',
-    'https://www.evga.com/products/product.aspx?pn=10G-P5-3883-KR',
-  ];
+async function checkAmazonProduct(url) {
+  try {
+    const $ = await fetchHTML(url);
+    const availabilitySpan = $('#availability span').text();
+    const availabilityText = availabilitySpan.replace(/[^0-9a-z]/gi, '').toLowerCase();
 
-  htmlUrls.forEach(async url => {
-    try {
-      const $ = await fetchHTML(url);
-      
-      const outOfStockDiv = $('#LFrame_pnlOutOfStock');
-  
-      if (outOfStockDiv.length > 0) {
-        return
-      }
-  
-      sendNotification('3080 In Stock', url)
-    } catch (error) {
-      console.log(error);
-      // Uncomment this if you want errors pushed to your device with pushover.
-      // sendNotification(`Error scraping products: ${error}`);
+    if (availabilityText == 'currentlyunavailable') {
+      return
     }
-  });
+
+    sendNotification('PS5 In Stock', url)
+  } catch (error) {
+    console.log(error);
+    // Uncomment this if you want errors pushed to your device with pushover.
+    // sendNotification(`Error scraping products: ${error}`);
+  }
 }
 
 exports.handler = function() {
-  checkEvga3080();
+  checkAmazonProduct('https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG');
 };
+
+exports.handler();
